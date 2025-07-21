e270689020f047e722558f55fc2d013d7f07e1ea
Zakria <zakriakhanz2003@gmail.com>
2025-07-21 15:38:17 +0500

    Implement authentication in Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,51 @@
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
-    return (
-        <header className="px-5 py-5 bg-white shadow-sm font-work-sans">
-            <nav className="flex justify-between items-center">
-                <Link href="/">
-                    <Image src="/logo.png" alt="logo" height={30} width={144} />
-                </Link>
-            </nav>
-        </header>
-    );
+const Navbar = async () => {
+
+    const session = await auth();
+
+  return (
+    <header className="px-5 py-5 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" height={30} width={144} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+            {session && session?.user ? (
+                <>
+                    <Link href={"/startup/create"}>
+                        <span>Create</span>
+                    </Link>
+
+                    <form action={ async ()=> {
+                        "use server";
+                        await signOut({redirectTo: "/"});
+                    }}>
+                        <button type="submit">Logout</button>
+                    </form>
+
+                    <Link href={`/user/${session?.id}`}>
+                        <span>{session?.user?.name}</span>
+                    </Link>
+                </>
+            ) : (
+                <form action={ async () => {
+                    "use server";
+                    await signIn('github');
+                    }}>
+                    <button type="submit">Login</button>
+                </form>
+            )}
+        </div>
+
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
