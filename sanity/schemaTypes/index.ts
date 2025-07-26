dcc885890cd722479708e5b831f9c0814e6532d2
Zakria <zakriakhanz2003@gmail.com>
2025-07-26 16:24:55 +0500

    Add author and startup schema types, integrate markdown support, and update dependencies

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { author } from "./author";
+import { startup } from "./startup";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, startup],
+};
