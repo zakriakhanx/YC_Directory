0e479e791512df03feb29bc1759728ac9cb597ec
Zakria <zakriakhanz2003@gmail.com>
2025-08-01 15:45:00 +0500

    Implement view tracking in View component and create write client for Sanity

--- components/View.tsx
+++ components/View.tsx
@@ -1,13 +1,30 @@
 import { STARTUP_VIEWS_QUERY } from "@/lib/queries";
 import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-    // todo: update the number of views
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
+  const viewString = () => {
+    if (totalViews <= 1) {
+      return "View";
+    } else {
+      return "Views";
+    }
+  };
+  const v = viewString();
 
   return (
     <div className="view-container">
@@ -16,7 +33,9 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">Views: {totalViews}</span>
+        <span className="font-black">
+          {totalViews} {v}
+        </span>
       </p>
     </div>
   );
