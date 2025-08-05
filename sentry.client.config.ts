286edc8834c5dee06e94e0ab80e952c351391b6c
Zakria Khan <zakriakhanz2003@gmail.com>
2025-08-06 00:52:52 +0500

    Update sentry.client.config.ts

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,9 +3,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://e4c37661070dbd0280ac5fadb1e33997@o4509785438289920.ingest.de.sentry.io/4509785444057168",
   integrations: [
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
+   //Sentry.feedbackIntegration({
+      //colorScheme: "system",
     }),
   ],
 });
