b5d65b97b62025c788cd33112cc4b501ea0031d3
Zakria <zakriakhanz2003@gmail.com>
2025-08-06 00:41:27 +0500

    update form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -41,7 +41,7 @@ const StartupForm = () => {
           description: "Your startup pitch has been created successfully",
         });
 
-        router.push(`/startup/${result._id}`);
+        // router.push(`/startup/${result._id}`);
       }
 
       return result;
